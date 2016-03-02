70b2f898ec54888b624b2f9f2080d3674e9485cb
Karim Bellioum <kbellioum@gmail.com>
2016-03-02 16:05:44 +0000

    cleaning code

--- routes/ping.js
+++ routes/ping.js
@@ -7,12 +7,10 @@ var exec = require('child_process').exec;
 
 router.get('/', function(req, res) {
 
-  //var username = req.query['ipaddr'];
 
   function puts(error, stdout, stderr) {
   	util.puts(stdout);
 
-  	//var txt = stdout.replace(/\n/g, "<br>");
 
   	res.render('pingresult', {toto: stdout.split(/\n/g)});
    }
